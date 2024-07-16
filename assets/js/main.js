6bb71de9cd11426e57663e147dc8b886230fa63d
arafatcnits <arafat.cnits@gmail.com>
2024-07-16 18:11:24 +0600

    back to button and all error solve

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,15 +60,14 @@ window.onresize = function(){
 
 
 new WOW({
-    boxClass: 'wow',      // default
-    animateClass: 'animate__animated', // default
-    offset: 0,          // distance to the element when triggering the animation (default is 0)
-    mobile: true,       // trigger animations on mobile devices (default is true)
-    live: true,       // act on asynchronously loaded content (default is true)
+    boxClass: 'wow',
+    animateClass: 'animate__animated',
+    offset: 0,
+    mobile: true,
+    live: true, 
     callback: function(box) {
-      // the callback is fired every time an animation is started
-      // the argument that is passed in is the DOM node being animated
     },
-    scrollContainer: null,    // optional scroll container selector, otherwise use window
-    resetAnimation: true,     // reset animation on end (default is true)
+    scrollContainer: null,
+    resetAnimation: true, 
   }).init();
+
